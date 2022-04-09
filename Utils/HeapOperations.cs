4db521a2ceaa52ba8c404de53623e3ef6e60644d
Shayan Toqraee <Shayan.To@Hotmail.com>
2022-04-09 14:45:10 +0430

    Utils: Add HeapOperations.

---
+++ Utils/HeapOperations.cs
@@ -0,0 +1,103 @@
+using System.Collections.Generic;
+
+namespace Utils
+{
+    public class HeapOperations<T>
+    {
+        public HeapOperations(IComparer<T> Comparer)
+        {
+            this.Comparer = Comparer;
+        }
+
+        public HeapOperations() : this(Comparer<T>.Default)
+        {
+        }
+
+        public void Heapify(IList<T> List, int Count, int Index)
+        {
+            while (true)
+            {
+                var MinIndex = LeftChild(Index);
+                if (MinIndex >= Count)
+                {
+                    break;
+                }
+
+                var Min = List[MinIndex];
+                var I = RightChild(Index);
+                T T;
+                if (I < Count)
+                {
+                    T = List[I];
+                    if (Comparer.Compare(T, Min) < 0)
+                    {
+                        MinIndex = I;
+                        Min = T;
+                    }
+                }
+
+                T = List[Index];
+                if (Comparer.Compare(Min, T) < 0)
+                {
+                    List[Index] = Min;
+                    List[MinIndex] = T;
+                    Index = MinIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public void MakeHeap(IList<T> List, int Count)
+        {
+            for (var I = Parent(Count - 1); I >= 0; I--)
+            {
+                Heapify(List, Count, I);
+            }
+        }
+
+        public void BubbleUp(IList<T> List, int Count, int Index)
+        {
+            while (true)
+            {
+                if (Index == 0)
+                {
+                    break;
+                }
+
+                var T = List[Index];
+                var PI = Parent(Index);
+                var P = List[PI];
+                if (Comparer.Compare(P, T) > 0)
+                {
+                    List[PI] = T;
+                    List[Index] = P;
+                    Index = PI;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public static int Parent(int Index)
+        {
+            return (Index - 1) / 2;
+        }
+
+        public static int LeftChild(int Index)
+        {
+            return 2 * Index + 1;
+        }
+
+        public static int RightChild(int Index)
+        {
+            return 2 * Index + 2;
+        }
+
+        private readonly IComparer<T> Comparer;
+    }
+}
