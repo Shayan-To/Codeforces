9305bc9507aa7d48e4e528be444367da5c159ab0
Shayan Toqraee <Shayan.To@Hotmail.com>
2024-02-17 18:25:55 +0330

    🏴 Add 1896.

---
+++ C1896/B.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Utils;
+
+using static Utils.Commons;
+
+// #util Commons
+// #util MathI
+
+namespace C1896
+{
+    public static class B
+    {
+        public static async Task Main()
+        {
+            await foreach (var (_, s) in In.ReadWordListAsync<int, string>(await In.ReadWordAsync<int>()))
+            {
+                var firstA = s.IndexOf('A');
+                var lastB = s.LastIndexOf('B');
+                firstA = firstA == -1 ? s.Length : firstA;
+                OutLine($"{Math.Max(0, lastB - firstA)}");
+            }
+        }
+    }
+}
