11c200dd4c2b28061a1896d4d36949a913620167
Shayan Toqraee <Shayan.To@Hotmail.com>
2024-02-17 18:25:55 +0330

    🏴 CodeTON Round 7 (1896)

---
+++ C1896/A.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Utils;
+
+using static Utils.Commons;
+
+// #util Commons
+// #util MathI
+
+namespace C1896
+{
+    public static class A
+    {
+        public static async Task Main()
+        {
+            await foreach (var n in In.ReadWordListAsync<int>(await In.ReadWordAsync<int>()))
+            {
+                var l = await In.ReadWordListAsync<int>(n).ToArrayAsync();
+                if (l.First() == 1)
+                {
+                    OutLine("Yes");
+                }
+                else
+                {
+                    OutLine("No");
+                }
+            }
+        }
+    }
+}
