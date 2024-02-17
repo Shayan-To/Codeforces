2ef254d7e9f0be7d832166ac52cbf8a2135520cd
Shayan Toqraee <Shayan.To@Hotmail.com>
2024-02-17 18:25:45 +0330

    🏴 TypeDB Forces 2023 (1787)

---
+++ C1787/B.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Utils;
+
+using static Utils.Commons;
+
+// #util Commons
+
+namespace C1787
+{
+    public static class B
+    {
+        public static async Task Main()
+        {
+            await foreach (var nn in In.ReadWordListAsync<int>(await In.ReadWordAsync<int>()))
+            {
+                var n = nn;
+                var l = new List<int>();
+
+                for (var k = 2; k * k <= n; k += 1)
+                {
+                    for (var i = 0; n % k == 0; i += 1)
+                    {
+                        n /= k;
+                        if (i < l.Count)
+                        {
+                            l[i] *= k;
+                        }
+                        else
+                        {
+                            l.Add(k);
+                        }
+                    }
+                }
+
+                if (n != 1)
+                {
+                    if (0 < l.Count)
+                    {
+                        l[0] *= n;
+                    }
+                    else
+                    {
+                        l.Add(n);
+                    }
+                }
+
+                ErrLine($"{l.JoinToString()}");
+
+                OutLine($"{l.Sum()}");
+            }
+        }
+    }
+}
