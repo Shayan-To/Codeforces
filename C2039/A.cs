74454e5517daa3f789c5e9c3c12cadead13441c1
Shayan Toqraee <Shayan.To@Hotmail.com>
2025-12-23 23:00:53 +0330

    🏴 CodeTON Round 9 (2039)

---
+++ C2039/A.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Utils;
+
+using static Utils.Commons;
+
+// #util Commons
+
+namespace C2039
+{
+    public static class A
+    {
+        public static async Task Main()
+        {
+            await foreach (var n in In.ReadWordListAsync<int>(await In.ReadWordAsync<int>()))
+            {
+                OutLine($"{RangeSC(1, n).Select(i => 2 * i - 1).JoinToString()}");
+            }
+        }
+    }
+}
---
+++ C2039/B.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Utils;
+
+using static Utils.Commons;
+
+// #util Commons
+
+namespace C2039
+{
+    public static class B
+    {
+        public static async Task Main()
+        {
+            await foreach (var s in In.ReadWordListAsync<string>(await In.ReadWordAsync<int>()))
+            {
+                var curP = new (int Prev, int Next)[s.Length];
+                var prevP = new (int Prev, int Next)[s.Length];
+
+                foreach (var i in Range(s.Length))
+                {
+                    if (curP[i] != (0, 0))
+                    {
+                        continue;
+                    }
+                    var ch = s[i];
+                    var p = i;
+                    var prev = -1;
+                    foreach (var j in RangeSE(i + 1, s.Length))
+                    {
+                        if (ch == s[j])
+                        {
+                            curP[p] = (prev, j);
+                            prev = j;
+                        }
+                    }
+                    curP[p] = (prev, -1);
+                }
+
+                // OutLine
+            }
+        }
+    }
+}
